9ccf2de41ac9144693a1b99c67ca99aae68ba507
ThanSoe <100922541+ThanSoe23@users.noreply.github.com>
2024-11-22 15:00:26 +0700

    error

---
+++ app/(root)/error.tsx
@@ -0,0 +1,40 @@
+"use client"; // Error components must be Client Components
+
+import { useEffect } from "react";
+
+import { Flex } from "@radix-ui/themes";
+import Image from "next/image";
+
+export default function CustomError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <Flex justify="center" align="center" direction="column" gap="4">
+      <Image
+        src="/assets/images/logo-text.svg"
+        alt="logo"
+        width={260}
+        height={54}
+      />
+      <div className="text-sm">
+        {error.message ??
+          "This page is under construction. Please come back later."}{" "}
+      </div>
+
+      <button
+        className="rounded bg-primary text-white py-2 px-4"
+        onClick={reset}
+      >
+        Back to Dashboard
+      </button>
+    </Flex>
+  );
+}
